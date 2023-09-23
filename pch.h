319decefeac9b8b2aadaba03fda6897c1fd22df2
Claudiu HBann <123uidualcnnamreh@gmail.com>
2023-09-23 23:47:53 +0300

    added a test project and made the streamable a static lib

--- pch.h
+++ pch.h
@@ -1,5 +1,6 @@
 #pragma once
 
+// std
 #include <algorithm>
 #include <sstream>
 
@@ -8,3 +9,19 @@ using size_range = uint32_t;
 template <typename> constexpr auto always_false = false;
 
 template <typename Type> using get_raw_t = std::remove_const_t<std::remove_reference_t<Type>>;
+
+// Streamable
+#include "Streamable/IStreamable.h"
+#include "Streamable/SizeFinder.h"
+#include "Streamable/Stream.h"
+#include "Streamable/StreamReader.h"
+#include "Streamable/StreamWriter.h"
+
+using namespace hbann;
+
+// Shared
+#define CATCH_AMALGAMATED_CUSTOM_MAIN
+
+#include "Shared/Catch2-3.4.0/catch_amalgamated.hpp"
+
+using namespace Catch;
