476c843b3c8dcb72c9b62a2cbe6c259a56c97143
Claudiu HBann <123uidualcnnamreh@gmail.com>
2023-09-24 16:56:07 +0300

    refactored StreamWriter type check and finished the streamable interface

--- Streamable/IStreamable.h
+++ Streamable/IStreamable.h
@@ -7,19 +7,14 @@ namespace hbann
 class IStreamable
 {
     friend class StreamWriter;
-    friend class StreamReader;
 
   public:
-    virtual Stream &ToStream()
-    {
-        return mStream;
-    }
+    virtual std::string_view ToStream() = 0;
+
+    virtual void FromStream(std::string_view aStream) = 0;
 
   protected:
-    virtual constexpr size_t FindParseSize() const noexcept
-    {
-        return 0;
-    }
+    virtual constexpr size_range FindParseSize() const noexcept = 0;
 
   private:
     Stream mStream;
--- Streamable/StreamWriter.h
+++ Streamable/StreamWriter.h
@@ -12,7 +12,9 @@ class StreamWriter
     template <typename Type, typename... Types>
     constexpr void WriteAll(Stream &aStream, const Type &aObject, const Types &...aObjects)
     {
-        Write(aStream, aObject);
+        using TypeRaw = get_raw_t<Type>;
+
+        Write<TypeRaw>(aStream, aObject);
 
         if constexpr (sizeof...(aObjects))
         {
@@ -20,11 +22,10 @@ class StreamWriter
         }
     }
 
+  private:
     template <typename Type> static inline void WriteObjectOfKnownSize(Stream &aStream, const Type &aObject)
     {
-        using TypeRaw = get_raw_t<Type>;
-
-        static_assert(std::is_standard_layout_v<TypeRaw> && !std::is_pointer_v<TypeRaw>,
+        static_assert(std::is_standard_layout_v<Type> && !std::is_pointer_v<Type>,
                       "Type is not an object of known size or it is a pointer!");
 
         const auto objectPtr = reinterpret_cast<const char *>(&aObject);
@@ -33,50 +34,51 @@ class StreamWriter
 
     static inline void WriteStreamable(Stream &aStream, IStreamable &aStreamable)
     {
-        const auto streamableSize = (size_range)aStreamable.FindParseSize();
-        WriteObjectOfKnownSize(aStream, streamableSize);
+        WriteObjectOfKnownSize(aStream, aStreamable.FindParseSize());
 
-        const auto &streamableParsedBuffer = aStreamable.ToStream().GetBuffer();
-        aStream.Write(streamableParsedBuffer.view().data(), streamableSize);
+        const auto stream = aStreamable.ToStream();
+        aStream.Write(stream.data(), stream.size());
     }
 
     template <typename Type> static constexpr void WriteRange(Stream &aStream, const Type &aRange)
     {
-        using TypeRaw = get_raw_t<Type>;
-
-        static_assert(std::ranges::range<TypeRaw>, "Type is not a range!");
+        static_assert(std::ranges::range<Type>, "Type is not a range!");
 
         WriteObjectOfKnownSize((size_range)std::ranges::size(aRange));
 
-        if constexpr (SizeFinder::FindRangeRank<TypeRaw>() > 1)
+        if constexpr (SizeFinder::FindRangeRank<Type>() > 1)
         {
-            std::ranges::for_each(aRange, [&](const auto &aObject) { WriteRange(aStream, aObject); });
+            for (const auto &aObject : aRange)
+            {
+                WriteRange(aStream, aObject);
+            }
         }
         else
         {
-            std::ranges::for_each(aRange, [&](const auto &aObject) { Write(aStream, aObject); });
+            for (const auto &aObject : aRange)
+            {
+                Write(aStream, aObject);
+            }
         }
     }
 
     template <typename Type> static constexpr void Write(Stream &aStream, const Type &aObject)
     {
-        using TypeRaw = get_raw_t<Type>;
-
-        if constexpr (std::is_standard_layout_v<TypeRaw> && !std::is_pointer_v<TypeRaw>)
+        if constexpr (std::is_standard_layout_v<Type> && !std::is_pointer_v<Type>)
         {
             WriteObjectOfKnownSize(aStream, aObject);
         }
-        else if constexpr (std::is_base_of_v<IStreamable, TypeRaw>)
+        else if constexpr (std::is_base_of_v<IStreamable, Type>)
         {
             WriteStreamable(aStream, aObject);
         }
-        else if constexpr (std::ranges::range<TypeRaw>)
+        else if constexpr (std::ranges::range<Type>)
         {
             WriteRange(aStream, aObject);
         }
         else
         {
-            static_assert(always_false<TypeRaw>, "Type is not accepted!");
+            static_assert(always_false<Type>, "Type is not accepted!");
         }
     }
 };
