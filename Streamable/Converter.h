ea8197303386dc49a23dfda16fb43e5bc006c76b
Claudiu HBann <123uidualcnnamreh@gmail.com>
2023-10-18 14:57:33 +0300

    now we use the deprecated standard way of converting between encodings

--- Streamable/Converter.h
+++ Streamable/Converter.h
@@ -11,46 +11,22 @@ namespace hbann
 class Converter
 {
   public:
-    [[nodiscard]] static inline std::string ToUTF8(const std::wstring &aString) noexcept
+    [[nodiscard]] static inline std::string ToUTF8(const std::wstring &aString)
     {
-        const auto requiredSize =
-            WideCharToMultiByte(CP_UTF8, 0, aString.c_str(), (int)aString.size(), nullptr, 0, nullptr, nullptr);
-        if (!requiredSize)
-        {
-            return {};
-        }
-
-        std::string stringUTF8(requiredSize, 0);
-        if (!WideCharToMultiByte(CP_UTF8, 0, aString.c_str(), (int)aString.size(), stringUTF8.data(), requiredSize,
-                                 nullptr, nullptr))
-        {
-            return {};
-        }
-
-        return stringUTF8;
+        return mConverter.to_bytes(aString);
     }
 
-    [[nodiscard]] static inline std::wstring FromUTF8(const std::span<const char> &aStringUTF8) noexcept
+    [[nodiscard]] static inline std::wstring FromUTF8(const std::span<const char> &aString)
     {
-        const auto requiredSize =
-            MultiByteToWideChar(CP_UTF8, 0, aStringUTF8.data(), (int)aStringUTF8.size(), nullptr, 0);
-        if (!requiredSize)
-        {
-            return {};
-        }
-
-        std::wstring string(requiredSize, 0);
-        if (!MultiByteToWideChar(CP_UTF8, 0, aStringUTF8.data(), (int)aStringUTF8.size(), string.data(), requiredSize))
-        {
-            return {};
-        }
-
-        return string;
+        return mConverter.from_bytes(aString.data(), aString.data() + aString.size());
     }
 
-    [[nodiscard]] static inline auto FromUTF8(const std::string &aStringUTF8) noexcept
+    [[nodiscard]] static inline auto FromUTF8(const std::string &aString)
     {
-        return FromUTF8(std::span<const char>{aStringUTF8});
+        return FromUTF8(std::span<const char>{aString});
     }
+
+  private:
+    static inline std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> mConverter{};
 };
 } // namespace hbann
--- pch.h
+++ pch.h
@@ -18,24 +18,16 @@ class StreamReader;
 } // namespace hbann
 
 // std
+#define _SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING
+
 #include <bit>
 #include <cmath>
+#include <codecvt>
 #include <memory>
 #include <span>
 #include <string>
 #include <variant>
 #include <vector>
-
-// OS specific
-#if defined(_WIN32) || defined(_WIN64)
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
-#include <Windows.h>
-#elif defined(__linux__)
-#error "Not implemented yet!"
-#else
-#error "Unknown OS"
-#endif
 
 // Streamable
 constexpr auto STREAMABLE_INTERFACE_NAME = "IStreamable";
@@ -116,7 +108,8 @@ template <typename Type>
 concept is_pointer = std::is_pointer_v<Type> || is_pointer_unique<Type> || is_pointer_shared<Type>;
 
 template <typename Type>
-concept is_std_lay_no_ptr = std::is_standard_layout_v<Type> && !is_pointer<Type>;
+concept is_std_lay_no_ptr = std::is_standard_layout_v<Type> && !
+is_pointer<Type>;
 
 template <typename Base, typename Derived>
 concept is_base_of_no_ptr = std::is_base_of_v<Base, std::remove_pointer_t<Derived>>;
@@ -125,19 +118,19 @@ template <typename Type>
 concept is_path = std::is_same_v<std::remove_cvref_t<Type>, std::filesystem::path>;
 
 template <typename Container>
-concept is_range_std_lay =
-    (std::ranges::contiguous_range<Container> && is_std_lay_no_ptr<typename Container::value_type>) ||
-    is_path<Container>;
+concept is_range_std_lay = (std::ranges::contiguous_range<Container> &&
+                            is_std_lay_no_ptr<typename Container::value_type>) ||
+                           is_path<Container>;
 
 template <typename Container>
 concept has_method_size = requires(Container &aContainer) { std::ranges::size(aContainer); };
 
 template <typename Class>
 concept has_method_find_derived_streamable = requires(StreamReader &aStreamReader) {
-    {
-        Class::FindDerivedStreamable(aStreamReader)
-    } -> std::convertible_to<IStreamable *>;
-};
+                                                 {
+                                                     Class::FindDerivedStreamable(aStreamReader)
+                                                     } -> std::convertible_to<IStreamable *>;
+                                             };
 
 constexpr bool static_equal(const char *aString1, const char *aString2) noexcept
 {
