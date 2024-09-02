c6f6f5b05b7ce46157f6fc0d15d5d2fb1f0aa6a3
Claudiu HBann <123uidualcnnamreh@gmail.com>
2024-09-02 20:45:16 +0300

    removed unused includes

--- Tests/pch.h
+++ Tests/pch.h
@@ -6,21 +6,9 @@
 // Shared
 #define CATCH_AMALGAMATED_CUSTOM_MAIN
 
-#include <Catch2-3.7.0/catch_amalgamated.hpp>
+#include "Catch2-3.7.0/catch_amalgamated.hpp"
 
 using namespace Catch;
-
-// nlohmann JSON
-#include <json-3.11.3/json.hpp>
-
-using namespace nlohmann;
-
-// msgpack
-#define MSGPACK_NO_BOOST
-
-#include <msgpack.hpp>
-
-using namespace msgpack;
 
 // Test
 #include <iostream>
